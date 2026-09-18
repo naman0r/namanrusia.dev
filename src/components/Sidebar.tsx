bd01f2b136205dd7aaa321e122b5c1e5c27ce6b1
Naman Rusia <naman.rusia14@gmail.com>
2026-09-18 11:12:17 -0400

    use rail sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,3 @@
 // Swap the sidebar by changing this one import.
 // Options: "./VscodeSidebar" (activity bar + explorer) or "./RailSidebar" (hover-expanding icon rail).
-export { default, MobileHeader, CONTENT_PADDING } from "./VscodeSidebar";
+export { default, MobileHeader, CONTENT_PADDING } from "./RailSidebar";
